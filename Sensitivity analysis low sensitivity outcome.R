217ee1ef16c161bea46db35473ffc46a87903323
joshehrenberg <83101940+joshehrenberg@users.noreply.github.com>
2023-02-21 11:57:45 -0500

    Remade analysis to change sensitivity instead of PPV

--- Sensitivity analysis low sensitivity outcome.R
+++ Sensitivity analysis low sensitivity outcome.R
@@ -1,66 +1,51 @@
 library(ggplot2)
 library(magrittr)
+library(purrr)
+library(survival)
 set.seed(108)
 
 #write a function to simulate data in each trial with 13500 patients
-simulate_trial <- function(death_rate_HCTZ, sensitivity) {
+simulate_trial <- function(event_rate_HCTZ, sensitivity) {
   #HR of 0.825
-  death_rate_CTD <- death_rate_HCTZ * 0.825
-  #block randomize with 6000 patients and block sizes of 6
+  event_rate_CTD <- event_rate_HCTZ * 0.825
+  #block randomize with 13500 patients and block sizes of 6
   treatment <- blockrand::blockrand(n = 13500, 
                        num.levels = 2,
                        levels = c("CTD", "HCTZ"),
                        block.sizes = 3)$treatment
   #Give hazard rate for each treatment
-  death_rate <- vector("numeric", length = 13500)
-  for(i in seq_len(13500)) {
-    death_rate[i] <- if(treatment[i] == "HCTZ") {
-      death_rate[i] <- death_rate_HCTZ
-    }
-    else {
-      death_rate[i] <- death_rate_CTD
-    }
-  }
-  #simulate true event times for every patient
-  true_event_time <- purrr::map_dbl(death_rate, ~rexp(1, .))
-  #simulate false positive event times for every patient
-  false_event_rate <- death_rate_HCTZ / sensitivity - death_rate_HCTZ
-  false_event_time <- rexp(13500, false_event_rate)
+  event_rate <- ifelse(treatment == "HCTZ", event_rate_HCTZ, event_rate_CTD)
+  #simulate event times for every patient
+  event_time <- purrr::map_dbl(event_rate, ~rexp(1, .))
+  #determine if the patient's event was counted
+  event_detected <- as.logical(rbinom(13500, 1, sensitivity))
+  counted_event_time <- purrr::map2_dbl(event_detected, event_time, ~ifelse(.x, .y, Inf))
   #determine which patients have the first 1055 events with the trial ending at 1055 events
-  first_event_time <- pmin(false_event_time, true_event_time, na.rm = TRUE)
-  trial_end_date <- sort(first_event_time)[1055]
-  follow_up_time <- pmin(first_event_time, trial_end_date)
-  event <- first_event_time <= follow_up_time
-  #determine if the trial detected an effect
+  last_counted_event_time <- sort(counted_event_time)[1055]
+  follow_up_time <- pmin(counted_event_time, last_counted_event_time)
+  event <- counted_event_time <= last_counted_event_time
+  #keep the data needed to make the PH model
   trial_data <- cbind.data.frame(treatment, follow_up_time, event)
   trial_data
 }
 
 #write a function that gives the percentage of 1000 trials that show a difference in the primary outcome (aka the power)
-simulate_trials <- function(death_rate_HCTZ, sensitivity) {
-  death_rates_HCTZ <- rep(death_rate_HCTZ, times = 1000)
-  sensitivities <- rep(sensitivity, times = 1000)
-  trials <- purrr::map2(death_rates_HCTZ, sensitivities, simulate_trial)
-  trials
+simulate_trial_power <- function(event_rate_HCTZ, sensitvity) {
+  pvalues <- 1:1000 %>%
+    map(~simulate_trial(event_rate_HCTZ, sensitivity)) %>%
+    map(~survdiff(Surv(follow_up_time, event) ~ treatment, .)) %>%
+    map("pvalue") %>%
+    map_lgl(~ 0.05 > .)
+  mean(pvalues)
 }
 
-#make a data frame of all sensitivities and death_rates for the analysis
-death_rate_HCTZ <- 0.03 / 365
+#make a vectors of sensitivity and death rate for each level to investigate
+event_rate_HCTZ <- 0.03 / 365
 sensitivity <- seq(0.5, 1, 0.01)
-power_data <- cbind.data.frame(death_rate_HCTZ, sensitivity)
-
 
 #determine the power at each death rate and sensitivity
-trial_power <- vector("list", length(sensitivity))
-for(i in seq_len(length(sensitivity))) {
-  trial_power[[i]] <- simulate_trials(death_rate_HCTZ, sensitivity[i]) %>%
-    purrr::map(~survival::survdiff(Surv(follow_up_time, event) ~ treatment, .)) %>%
-    purrr::map("pvalue") %>%
-    purrr::map_lgl(~ 0.05 > .) %>%
-    mean()
-}
-trial_power <- unlist(trial_power)
-graph_data <- cbind.data.frame(sensitivity, trial_power)
+trial_power <- map_dbl(sensitivity, ~simulate_trial_power(event_rate_HCTZ, .))
+graph_data <- cbind.data.frame(senstivitity, trial_power)
 
 #graph the results
 ggplot(graph_data, aes(x = sensitivity, y = trial_power)) +
@@ -69,7 +54,6 @@ ggplot(graph_data, aes(x = sensitivity, y = trial_power)) +
   scale_x_reverse() +
   scale_y_continuous(breaks = seq(0,1,0.1), limits = c(0,1)) +
   ylab("Power") +
-  xlab("Sensitivity in HCTZ group") +
+  xlab("Sensitivity") +
   theme_minimal()
-
 
